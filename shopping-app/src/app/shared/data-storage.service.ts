ebef6a281f82621487b1ee85e0097d7a8c151ecb
aparnanambissan <33481201+aparnanambissan@users.noreply.github.com>
2020-04-16 18:46:23 -0400

    Added Authentication Module

--- shopping-app/src/app/shared/data-storage.service.ts
+++ shopping-app/src/app/shared/data-storage.service.ts
@@ -2,19 +2,20 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { RecipeService } from '../recipes/recipe.service';
 import { Recipe } from '../recipes/recipe.model';
-import { Observable } from 'rxjs';
-import { catchError, map, tap } from 'rxjs/operators';
+import { AuthService } from '../auth/auth.service';
 
 @Injectable()
 export class DataStorageService {
-    constructor(private http: HttpClient, private recipeService: RecipeService) { }
+    constructor(private http: HttpClient, private recipeService: RecipeService, private authService: AuthService) { }
 
     storeRecipes() {
-        return this.http.put('https://ng-recipe-book-bcef5.firebaseio.com/recipes.json', this.recipeService.getRecipes());
+        const token = this.authService.getToken();
+        return this.http.put('https://ng-recipe-book-bcef5.firebaseio.com/recipes.json?auth=' + token, this.recipeService.getRecipes());
     }
 
     getRecipes() {
-        this.http.get<any>('https://ng-recipe-book-bcef5.firebaseio.com/recipes.json')
+        const token = this.authService.getToken();
+        this.http.get<any>('https://ng-recipe-book-bcef5.firebaseio.com/recipes.json?auth=' + token)
             .subscribe(
                 data => {
                     const recipes: Recipe[] = data;
